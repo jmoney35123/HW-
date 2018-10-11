d8aa74745e0a717a360a76ec3f13af0cc94b3246
jmoney35123 <javonwilliams786@gmail.com>
2018-10-11 09:12:56 -0400

    hw

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+document.getElementById("kok").innerHtml= "Nice Females";
+'use strict'
+ document.getElementById("test").innerHTML = "I'm rich bitch!!";
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = new Date();
